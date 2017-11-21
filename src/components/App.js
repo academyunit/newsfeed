7d55c6977d9a41b33ee7076b9638422fe62e8cc6
k <admin@localhost>
2017-11-21 02:49:58 -0800

    add react-select control, install style-loader and css-loader

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React, {Component, PropTypes} from 'react';
 import ArticleList from './ArticleList';
 import Chart from './Chart';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
 
 class App extends Component {
   static propTypes = {
@@ -15,13 +17,20 @@ class App extends Component {
     super();
 
     this.state = {
-      text: ''
+      text: '',
+      selected: null
     };
   }
 
   render() {
+    const options = this.props.articles.map(article => ({
+      value: article.id,
+      label: article.title
+    }));
+
     return (
       <div>
+        <Select name="test-select" options={options} value={this.state.selected} onChange={this.handleSelectChange} multi={true}/>
         Enter your name: <input type="text" value={this.state.text} onChange={this.handleTextChange}/>
         <ArticleList articles={this.props.articles}/>
         <Chart articles={this.props.articles}/>
@@ -40,6 +49,10 @@ class App extends Component {
       text: text
     });
   };
+
+  handleSelectChange = selected => {
+    this.setState({selected});
+  }
 }
 
 export default App;
